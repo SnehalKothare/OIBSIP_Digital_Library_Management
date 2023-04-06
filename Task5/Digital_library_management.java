8db0823a1f7506902974e089c9a82c3bc9db8a8c
SnehalKothare <128027092+SnehalKothare@users.noreply.github.com>
2023-04-06 19:00:28 +0530

    Add files via upload

---
+++ Task5/Digital_library_management.java
@@ -0,0 +1,21 @@
+package Task5;
+public class Digital_library_management {
+    public static void main(String[] args) {
+        methods obj=new methods();
+        for (; ; ) {
+            int choice = obj.userInput();
+            switch (choice) {
+                case 0 -> {
+                    System.out.println("Library Closed!");
+                    System.exit(0);
+                }
+                case 1 -> obj.printAvailableBook();
+                case 2 -> obj.issueBook();
+                case 3 -> obj.returnBook();
+                case 4 -> obj.printIssuedBook();
+                case 5 -> obj.addBook();
+                default -> System.out.println("Invalid Input!");
+            }
+        }
+    }
+}
